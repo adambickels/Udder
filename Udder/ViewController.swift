1487f20263c25707a91131aec902044d2e2b9072
Adam Bickels <abickels@gmail.com>
2020-04-25 18:02:24 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Adam Bickels on 25/04/2020.
+//  Copyright © 2020 Adam Bickels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
